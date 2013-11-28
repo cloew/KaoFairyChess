7d393628e4f859fc94d66624c42dbe11d0f4a8f2
Chris Loew <cloew123@gmail.com>
2013-11-28 12:16:43 -0700

    Added a stubbed-out Game Type

--- src/Game/Test/suite.py
+++ src/Game/Test/suite.py
@@ -1,6 +1,8 @@
 import unittest
 
+from Game.Type.Test.suite import suite as type_suite
 from Game.Player.Test.suite import suite as player_suite
 
-suites = [player_suite]
+suites = [player_suite,
+          type_suite]
 suite = unittest.TestSuite(suites)
---
+++ src/Game/Type/game_type.py
@@ -0,0 +1,7 @@
+
+class GameType:
+    """ Represents a Chess Game Type """
+    
+    def __init__(self):
+        """ Initialize the Game Type """
+        self.numberOfPlayers = 2
