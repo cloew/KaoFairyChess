3a2f1d7fe233dfc99b5249ed03adba8eeee82a9f
Chris Loew <cloew123@gmail.com>
2013-11-28 12:31:39 -0700

    Added stubbed out Player Preparer

---
+++ src/Game/Player/player_preparer.py
@@ -0,0 +1,9 @@
+
+class PlayerPreparer:
+    """ Prepares a Player for a game of chess """
+    
+    def __init__(self):
+        """ Initialize the Player Preparer """
+        self.playerOnTop = False
+        
+    
