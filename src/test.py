f90ceba81cbc6e14d4a76da27857e84ae932e49c
Chris Loew <cloew123@gmail.com>
2013-11-28 12:17:00 -0700

    Hooked Game Tests into the core test.py

--- src/test.py
+++ src/test.py
@@ -1,9 +1,11 @@
 import unittest
 
+from Game.Test.suite import suite as game_suite
 from Board.Test.suite import suite as board_suite
 
 # Collect all the test suites
-suites = [board_suite]
+suites = [board_suite,
+          game_suite]
 
 alltests = unittest.TestSuite(suites)
 
