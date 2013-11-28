2d8e4a8a35f824c1685f99c2a496c6243e525e23
Chris Loew <cloew123@gmail.com>
2013-11-28 11:56:27 -0700

    Added PieceOnBoard class to represent a piece on the baord

---
+++ src/Board/piece_on_board.py
@@ -0,0 +1,8 @@
+
+class PieceOnBoard:
+    """ Represents a Piece on the Board """
+    
+    def __init__(self, piece, tile):
+        """ Initialize the Piece on the Board """
+        self.piece = piece
+        self.tile = tile
