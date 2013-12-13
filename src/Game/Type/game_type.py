42fe61d1c347c55fb30c620e7a5539cb255dc08c
Chris Loew <cloew123@gmail.com>
2013-12-12 20:50:00 -0700

    Filled out some of the Game Type logic

--- src/Game/Type/game_type.py
+++ src/Game/Type/game_type.py
@@ -1,10 +1,23 @@
+from Game.Player.player import Player
+
+from Game.Player.black_player_preparer import BlackPlayerPreparer
+from Game.Player.white_player_preparer import WhitePlayerPreparer
 
 class GameType:
     """ Represents a Chess Game Type """
     
     def __init__(self):
         """ Initialize the Game Type """
-        self.playerPreparers = []
+        self.playerPreparers = [WhitePlayerPreparer(), BlackPlayerPreparer()]
+        
+    def getPlayersForGame(self, board):
+        """ Return all the players for use in the game """
+        players = []
+        for preparer in self.playerPreparers:
+            player = Player()
+            preparer.prepare(player, board)
+        
+        return players
         
     @property
     def numberOfPlayers(self):
