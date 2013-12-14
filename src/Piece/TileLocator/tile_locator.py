f85bc3aa12e2d7adb4d4d7c6f5142488c1dbf18c
Chris Loew <cloew123@gmail.com>
2013-12-14 12:00:48 -0700

    Stubbed out the tile locator

--- src/Piece/TileLocator/tile_locator.py
+++ src/Piece/TileLocator/tile_locator.py
@@ -1,3 +1,10 @@
 
 class TileLocator:
-    """ ADD CLASS DESCRIPTION """
+    """ Represents a method for locating tiles on the board """
+    
+    def __init__(self):
+        """ Initialize the Tile Locator """
+        
+    def locateTiles(self, startingTile, board):
+        """ Locate and return all potential tiles based on the location algorithm """
+        return []
