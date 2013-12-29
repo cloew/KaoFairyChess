ef685eeb3ef3c8f5b12401a713cf6b28b47bb454
Chris Loew <cloew123@gmail.com>
2013-12-29 15:37:18 -0700

    The Piece now defaults to Pawn style movement

--- src/Piece/piece.py
+++ src/Piece/piece.py
@@ -1,3 +1,6 @@
+from Piece.Movement.movement_delegate import MovementDelegate
+from Piece.TileFilter.empty_tile_filter import EmptyTileFilter
+from Piece.TileLocator.vector_tile_locator import VectorTileLocator
 
 class Piece:
     """ Represents a chess piece """
@@ -6,6 +9,9 @@ class Piece:
         """ Initialize the piece """
         self.symbol = symbol
         self.color = color
+        locator = VectorTileLocator((1, 0))
+        filter = EmptyTileFilter()
+        self.movementDelegate = MovementDelegate(locator, [filter]) # Eventually this will get passed in
         
     def __repr__(self):
         """ Return the String Representation of the piece """
